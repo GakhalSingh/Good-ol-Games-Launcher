57e25140ed79f024a7e6b47cfe39049995ce6d42
brian <briansuyker@gmail.com>
2024-04-08 12:10:13 +0200

    heb wat classes aan gemaakt om de main uit te te dunnen

--- app/src/main/java/gogl/Main.java
+++ app/src/main/java/gogl/Main.java
@@ -4,27 +4,92 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+class HomePage{
+    int number;
+    public HomePage(){
+    System.out.println("Welkom in de GoG dashboard!");
+    System.out.println("------------------------------");
+    System.out.println("");
+    System.out.println("    ##          ##");
+    System.out.println("      ##      ##");
+    System.out.println("    ##############");
+    System.out.println("  ####  ######  ####");
+    System.out.println("######################");
+    System.out.println("##  ##############  ## ");
+    System.out.println("##  ##          ##  ##");
+    System.out.println("      ####  ####");
+    System.out.println("");
+    System.out.println("------------------------------");
+    System.out.println("Selecteer een menu:");
+    System.out.println("1 - Ranglijsten");
+    System.out.println("2 - Reviews");
+    System.out.println("3 - Sales");
+
+    number = scanner.nextInt();
+    }
+}
+
+class Ranglijsten{
+    int number;
+
+    public MenuRanglijst() {
+    System.out.println("Selecteer een sorteer methode.");
+    System.out.println("1 - Geen");
+    System.out.println("2 - Rating Hoog - Laag");
+    System.out.println("3 - Rating Laag - Hoog");
+    System.out.println("4 - Terug naar menu");
+    number = scanner.nextInt();
+
+    switch (number) {
+        case 1:
+            try {
+                File file = new File("data/ranglijst.csv");
+                Scanner scanner = new Scanner(file);
+
+                while (scanner.hasNextLine()) {
+                    String data = scanner.nextLine();
+                    System.out.println(data);
+                }
+
+                scanner.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("CSV bestand niet gevonden 😔");
+                HomePage();
+            }
+            break;
+        case 2:
+//                komt zo
+            HomePage();
+            break;
+        case 3:
+            // wip
+            HomePage();
+            break;
+        case 4:
+            HomePage();
+            break;
+        default:
+            System.out.println("Invoer niet herkent, probeer het opnieuw.");
+            showMenuRanglijst();
+    }
+}
+
+}
+
+
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welkom in de GoG dashboard!");
-        showMenu();
-    }
-
-    public static void showMenu() {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println();
-        System.out.println("Selecteer een menu: (1/2/3)");
-        System.out.println("1 - Ranglijsten");
-        System.out.println("2 - Reviews");
-        System.out.println("3 - Sales");
-
-        int number = scanner.nextInt();
+        HomePage homePage = new HomePage();
+        Ranglijsten ranglijsten = new Ranglijsten();
+        homePage();
 
         switch (number) {
             case 1:
 
-                showMenuRanglijst();
+                ranglijsten;
                 break;
             case 2:
                 showMenuReviews();
@@ -40,50 +105,7 @@ public class Main {
         scanner.close();
     }
 
-    public static void showMenuRanglijst() {
 
-        Scanner scannernumber = new Scanner(System.in);
-
-        System.out.println("Selecteer een sorteer methode.");
-        System.out.println("1 - Geen");
-        System.out.println("2 - Rating Hoog - Laag");
-        System.out.println("3 - Rating Laag - Hoog");
-        System.out.println("4 - Terug naar menu");
-        int number = scannernumber.nextInt();
-
-        switch (number) {
-            case 1:
-                try {
-                    File file = new File("data/ranglijst.csv");
-                    Scanner scanner = new Scanner(file);
-
-                    while (scanner.hasNextLine()) {
-                        String data = scanner.nextLine();
-                        System.out.println(data);
-                    }
-
-                    scanner.close();
-                } catch (FileNotFoundException e) {
-                    System.out.println("CSV bestand niet gevonden 😔");
-                    showMenu();
-                }
-                break;
-            case 2:
-//                komt zo
-                showMenu();
-                break;
-            case 3:
-                // wip
-                showMenu();
-                break;
-            case 4:
-                showMenu();
-                break;
-            default:
-                System.out.println("Invoer niet herkent, probeer het opnieuw.");
-                showMenuRanglijst();
-        }
-    }
 
     public static void showMenuReviews() {
         // wip
